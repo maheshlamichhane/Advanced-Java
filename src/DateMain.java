89f562d20756d24998f2aa936edf1ef0b813a0da
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-11-13 00:23:26 +0545

    java date seaction completed

---
+++ src/DateMain.java
@@ -0,0 +1,45 @@
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DateMain {
+
+    public static void main(String[] args) throws SQLException, ParseException, ClassNotFoundException {
+
+        // Inserting Date
+        insertingDate();
+
+        // Retrieving Data
+        retrievingDate();
+
+        java.util.Date uDate = new java.util.Date();
+        System.out.println("UDate ="+uDate);
+        long l = uDate.getTime();
+        java.sql.Date sDate = new java.sql.Date(l);
+        System.out.println("SqlDate="+sDate);
+    }
+
+    public static void insertingDate() throws ParseException, ClassNotFoundException, SQLException {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        java.util.Date utilDate = format.parse("20/04/1994");
+
+        Connection con = DBUtil.getConnection();
+        PreparedStatement pst = con.prepareStatement("insert into info values(?,?)");
+        pst.setString(1,"Zero");
+        pst.setDate(2,new java.sql.Date(utilDate.getTime()));
+        int x = pst.executeUpdate();
+        if(x == 1) {
+            System.out.println("Insertion Successful");
+        }
+    }
+
+    public static void retrievingDate() throws SQLException, ClassNotFoundException {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Connection con = DBUtil.getConnection();
+        PreparedStatement pst = con.prepareStatement("select * from info");
+        ResultSet rs = pst.executeQuery();
+        while(rs.next()) {
+            System.out.println(rs.getString(1)+"..."+format.format(rs.getDate(2)));
+        }
+    }
+}
